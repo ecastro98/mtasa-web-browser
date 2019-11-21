f4da6658a8bf6f72c41401fa965201941ca292bb
Emiliano Castro <emilianocast98@gmail.com>
2019-11-21 11:01:56 -0300

    Enable direct connect

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
+function isValidIpv4Addr(ip) {
+    return /^(?=\d+\.\d+\.\d+\.\d+$)(?:(?:25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]|[0-9])\.?){4}$/.test(ip);
+}
+
 new Vue({
     el: '#app',
     data: {
+        directIp: '',
         search: '',
         servers: [
             { version: "...", name: "Loading...", players: 0, maxplayers: "0", ip: "0", port: 0 },
@@ -24,6 +29,21 @@ new Vue({
     methods: {
         openServer(server) {
             window.location.href = `mtasa://${server.ip}:${server.port}`;
+        },
+        connectToIp() {
+            const [ip, port] = this.directIp.split(':');
+
+            if (!isValidIpv4Addr(ip)) {
+                alert('This isn\'t a valid IP');
+                return;
+            }
+
+            if (!port) {
+                alert('You\'re missing the port');
+                return;
+            }
+
+            window.location.href = `mtasa://${this.directIp}`;
         }
     }
 })
